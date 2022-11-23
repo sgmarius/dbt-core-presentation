b02caad143c188da88eebbfd5ade4910f6e5e4e7
Marius <gsmarius93@gmailc.com>
2022-11-23 23:50:52 +0200

    some cheeky stuff

--- start-superset.sh
+++ start-superset.sh
@@ -1,3 +1,4 @@
+# too lazy to add this to create a docker image doing this ahahhahahah SYKE.
 docker pull apache/superset
 docker run -d -p 8088:8088 --name superset apache/superset
 docker exec -it superset superset fab create-admin \
@@ -7,5 +8,5 @@ docker exec -it superset superset fab create-admin \
               --email admin@superset.com \
               --password admin
 docker exec -it superset superset db upgrade
-docker exec -it superset superset load_examples
+# docker exec -it superset superset load_examples # maybe this is a bad idea.
 docker exec -it superset superset init
